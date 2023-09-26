db66496f05f8caadb59125a5179ce2a829fe97ba
blockchainGuru125 <kkh0102www@outlook.com>
2023-09-26 18:12:21 +1000

    first commit

---
+++ projects/router/hardhat.config.ts
@@ -0,0 +1,170 @@
+import type { HardhatUserConfig, NetworkUserConfig } from 'hardhat/types'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-web3'
+import '@nomiclabs/hardhat-truffle5'
+import 'hardhat-abi-exporter'
+import 'hardhat-contract-sizer'
+import 'dotenv/config'
+import 'hardhat-tracer'
+import '@nomiclabs/hardhat-etherscan'
+import 'solidity-docgen'
+require('dotenv').config({ path: require('find-config')('.env') })
+
+// const pulseTestnet: NetworkUserConfig = {
+//   url: 'https://rpc.ankr.com/bsc_testnet_chapel',
+//   chainId: 943,
+//   accounts: [process.env.KEY_TESTNET!],
+// }
+
+// const goerli: NetworkUserConfig = {
+//   url: `https://eth-goerli.g.alchemy.com/v2/${process.env.GOERLI_API_KEY}`,
+//   chainId: 5,
+//   // accounts: [process.env.KEY_GOERLI!],
+// }
+
+// const bscMainnet: NetworkUserConfig = {
+//   url: 'https://bsc-dataseed.binance.org/',
+//   chainId: 56,
+//   // accounts: [process.env.KEY_MAINNET!],
+// }
+
+const pulseTestnet: NetworkUserConfig = {
+  url: 'https://rpc.v4.testnet.pulsechain.com/',
+  chainId: 943,
+  accounts: [process.env.KEY_TESTNET!],
+}
+
+const bscMainnet: NetworkUserConfig = {
+  url: 'https://bsc-dataseed.binance.org/',
+  chainId: 56,
+  accounts: [process.env.KEY_MAINNET!],
+}
+
+const goerli: NetworkUserConfig = {
+  url: 'https://rpc.ankr.com/eth_goerli',
+  chainId: 5,
+  accounts: [process.env.KEY_GOERLI!],
+}
+
+const eth: NetworkUserConfig = {
+  url: 'https://eth.llamarpc.com',
+  chainId: 1,
+  accounts: [process.env.KEY_ETH!],
+}
+
+const config: HardhatUserConfig = {
+  defaultNetwork: 'hardhat',
+  networks: {
+    hardhat: {
+      forking: {
+        url: pulseTestnet.url || '',
+      },
+    },
+    ...(process.env.KEY_TESTNET && { pulseTestnet }),
+    ...(process.env.KEY_MAINNET && { bscMainnet }),
+    ...(process.env.KEY_GOERLI && { goerli }),
+    ...(process.env.KEY_ETH && { eth }),
+    // goerli: goerli,
+    // mainnet: bscMainnet,
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY || '',
+    customChains: [
+      {
+        network: "pulseTestnet",
+        chainId: 943,
+        urls: {
+          apiURL: "https://scan.v4.testnet.pulsechain.com/api",
+          browserURL: "https://scan.v4.testnet.pulsechain.com/"
+        }
+      }
+    ],
+  },
+  solidity: {
+    compilers: [
+      {
+        version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10,
+          },
+        },
+      },
+      {
+        version: '0.8.10',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10,
+          },
+        },
+      },
+      {
+        version: '0.6.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10,
+          },
+        },
+      },
+      {
+        version: '0.5.16',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10,
+          },
+        },
+      },
+      {
+        version: '0.4.18',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 10,
+          },
+        },
+      },
+    ],
+    overrides: {
+      '@pancakeswap/v3-core/contracts/libraries/FullMath.sol': {
+        version: '0.7.6',
+        settings: {},
+      },
+      '@pancakeswap/v3-core/contracts/libraries/TickBitmap.sol': {
+        version: '0.7.6',
+        settings: {},
+      },
+      '@pancakeswap/v3-core/contracts/libraries/TickMath.sol': {
+        version: '0.7.6',
+        settings: {},
+      },
+      '@pancakeswap/v3-periphery/contracts/libraries/PoolAddress.sol': {
+        version: '0.7.6',
+        settings: {},
+      },
+      'contracts/libraries/PoolTicksCounter.sol': {
+        version: '0.7.6',
+        settings: {},
+      },
+    },
+  },
+  paths: {
+    sources: './contracts',
+    tests: './test',
+    cache: './cache',
+    artifacts: './artifacts',
+  },
+  // abiExporter: {
+  //   path: "./data/abi",
+  //   clear: true,
+  //   flat: false,
+  // },
+  docgen: {
+    pages: 'files',
+  },
+}
+
+export default config
